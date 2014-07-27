f0b130851992ce52993e3dd01e3069e494828e5a
Alex Flores <virtualsnyper@gmail.com>
2014-07-27 18:18:27 -0400

    add domain match url
    - removed url match conditional at the end of the file
    - 4space indent pass

--- FirstRow.user.js
+++ FirstRow.user.js
@@ -3,44 +3,47 @@
 // @namespace  http://alexflor.es/
 // @version    0.2
 // @description  Minor Cleanup and ad removal. Note: firstrow uses a shitton of nested iframes, making simple scripts impossible. You'll still have to manually close overlays on the video at site launch. Most of the future ones might be disabled. Having a good hosts file helps too. I got a good ad free firstrow with this script and this terminal command (macosx) curl -s http://winhelp2002.mvps.org/hosts.txt >> /etc/hosts
-// @match      http://*/*
+// @match      http://ifirstrowus.eu/watch/*
 // @copyright  2012+, You
 // ==/UserScript==
 
 var CleanUp = {
-  init: function(){
-    this.stopTimers();
-    this.clearSpace();
-    this.clearAds();
-  },
+    init: function(){
+        this.stopTimers();
+        this.clearSpace();
+        this.clearAds();
+    },
 
-  stopTimers: function() {
-    for (var i = 1; i < 99999; i++) { window.clearInterval(i);}
-  },
 
-  clearAds: function() {
-    var el = [ "ad_footer_content", "ad_footer", "ad_overlay_content", "ad_overlay", "banner_container" ];
+    stopTimers: function() {
+        for (var i = 1; i < 99999; i++) { window.clearInterval(i);}
+    },
 
-    for (var i = 0; i < el.length; i++) {
-      try {
-        document.getElementById(el[i]).remove();
-      }
-      catch(e) {
-        console.log( el[i] + " => This element does not currently exist" );
-      }
+
+    // Only clears player ad divs created by firstrow, not ones placed in iframes
+    clearAds: function() {
+        var el = [ "ad_footer_content", "ad_footer", "ad_overlay_content", "ad_overlay", "banner_container" ];
+
+        for (var i = 0; i < el.length; i++) {
+            try {
+                document.getElementById(el[i]).remove();
+            }
+            catch(e) {
+                console.log( el[i] + " => This element does not currently exist" );
+            }
+        }
+    },
+
+
+    // Removes side bar and banners
+    clearSpace: function() {
+        var allTableData = document.getElementsByTagName('td');
+        for (var i = 0; i < allTableData.length; i++) {
+            allTableData[i].getAttribute('width') === "350" ? allTableData[i].remove() : false
+        }
+        document.getElementsByTagName('tr')[3].remove();
+        document.getElementsByTagName('tr')[1].remove();
     }
-  },
-
-  clearSpace: function() {
-    var allTableData = document.getElementsByTagName('td');
-    for (var i = 0; i < allTableData.length; i++) {
-      allTableData[i].getAttribute('width') === "350" ? allTableData[i].remove() : false
-    }
-    document.getElementsByTagName('tr')[3].remove();
-    document.getElementsByTagName('tr')[1].remove();
-  }
 };
 
-if (window.location.href.match(/watch/)) {
-  CleanUp.init();
-}
+CleanUp.init();
